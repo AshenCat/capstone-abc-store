cc93d8624fce88b31d92c16ad78aaa3b76d8f778
Klifford Agujar <forddagujar95@gmail.com>
2020-02-25 22:29:25 -0500

    cleaned some useless functions

--- src/components/pages/nav/Navs.js
+++ src/components/pages/nav/Navs.js
@@ -4,82 +4,17 @@ import { Link } from 'react-router-dom'
 
 export default class Navs extends Component {
     state = {
-
-    }
-
-    isList = () => {
-        if(window.location.pathname === "/home")
-            return true
-        return false
-    }
-
-    //department manager
-    isAddItem = () => {
-        if(window.location.pathname === "/add-item")
-            return true
-        return false
-    }
-
-    isTickets = () => {
-        if(window.location.pathname === "/tickets")
-            return true
-        return false
-    }
-
-    isStats = () => {
-        if(window.location.pathname === "/stats")
-            return true
-        return false
-    }
-
-    isUsers = () => {
-        if(window.location.pathname === "/users")
-            return true
-        return false
-    }
-
-    //Store Clerk
-
-    isRequestItems = () => {
-        if(window.location.pathname === "/request-items")
-            return true
-        return false
-    }
-
-    isCustomerReturns = () => {
-        if(window.location.pathname === "/customer-returns")
-            return true
-        return false
-    }
-
-    //Warehouse Associate
-
-    isInvoices = () => {
-        if(window.location.pathname === "/invoices")
-            return true
-        return false
-    }
-
-    isReceiveShipments = () => {
-        if(window.location.pathname === "/receive-shipments")
-            return true
-        return false
-    }
-
-    isVendorReturn = () => {
-        if(window.location.pathname === "/vendor-return")
-            return true
-        return false
+        isList: false
     }
 
     ViewManager = () => {
         if (this.props.access === "Department Manager"){
             return (
                 <React.Fragment>
-                    <ListGroup.Item as="li" className="text-center"><Link to="/add-item" style={{color: 'black'}}>Add Item</Link></ListGroup.Item>
-                    <ListGroup.Item as="li" className="text-center"><Link to="/tickets" style={{color: 'black'}}>Request Tickets</Link></ListGroup.Item>
-                    <ListGroup.Item as="li" className="text-center"><Link to="/stats" style={{color: 'black'}}>Item Statistics</Link></ListGroup.Item>
-                    <ListGroup.Item as="li" className="text-center"><Link to="/users" style={{color: 'black'}}>Users</Link></ListGroup.Item>
+                    <ListGroup.Item as="li" className="text-center"><Link to="/add-item" >Add Item</Link></ListGroup.Item>
+                    <ListGroup.Item as="li" className="text-center"><Link to="/tickets" >Request Tickets</Link></ListGroup.Item>
+                    <ListGroup.Item as="li" className="text-center"><Link to="/stats" >Item Statistics</Link></ListGroup.Item>
+                    <ListGroup.Item as="li" className="text-center"><Link to="/users">Users</Link></ListGroup.Item>
                 </React.Fragment>
             )
         }
@@ -95,7 +30,7 @@ export default class Navs extends Component {
             return (
                 <React.Fragment>
                     <ListGroup.Item as="li" className="text-center"><Link to="/invoices">Invoices</Link></ListGroup.Item>
-                    <ListGroup.Item as="li" className="text-center"><Link to="/receive-shipments">Receive Shipments</Link></ListGroup.Item>
+                    <ListGroup.Item as="li" className="text-center"><Link to="/receive-shipment">Receive Shipments</Link></ListGroup.Item>
                     <ListGroup.Item as="li" className="text-center"><Link to="/vendor-return">Vendor Return</Link></ListGroup.Item>
                 </React.Fragment>
             )
@@ -105,7 +40,8 @@ export default class Navs extends Component {
     render() {
         return (
             <ListGroup as="ul">
-                <ListGroup.Item as="li"  className="text-center" active={this.isList()}><Link to="/home" style={{color: 'black'}}>Inventory List</Link></ListGroup.Item>
+                <ListGroup.Item className="text-center" style={{"background":"#d3d3d3"}}><h5>Navigation</h5></ListGroup.Item>
+                <ListGroup.Item as="li"  className="text-center"><Link  to="/home">Inventory List</Link></ListGroup.Item>
                 <this.ViewManager/>
             </ListGroup>
         )
