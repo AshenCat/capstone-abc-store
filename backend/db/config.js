ad14d763e291975a591424ca0de0c7158ea87107
Igor Belem <igortbelem@gmail.com>
2020-05-14 11:04:44 -0400

    added dotenv

--- backend/db/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    db: 'mongodb://localhost:27017/abc-store-db',
-    remote: 'mongodb+srv://admin:admin@inventorymanagamentsystem-yfwog.mongodb.net/test?retryWrites=true&w=majority'
-}
